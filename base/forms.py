e90c942f898cbfde6607a60dcc6771e4eb02fc9d
mubaarock <mustophamubarack17@gmail.com>
2024-03-06 14:10:03 +1100

    Validate uploaded Image
    check if uploaded image's size is not larger than 100kb
    using clean_image() method.

--- base/forms.py
+++ base/forms.py
@@ -1,8 +1,20 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Photo
 
 
-class PhotoModelForm(ModelForm):
+class PhotoModelForm(forms.ModelForm):
     class Meta:
         model = Photo
         fields = ["short_title", "image"]
+
+    def clean_image(self):
+        """Checks if image size is less than
+        a 100kilobyte
+        """
+        image = self.cleaned_data["image"]
+        image_size_kb = image.size / 2**10
+        if image_size_kb > 100:
+            raise forms.ValidationError(
+                "Image size should not be more thank 100 kilobytes."
+            )
+        return image
