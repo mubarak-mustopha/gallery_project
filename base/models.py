c242a328cc5a789a05e9fdde883ebf03aefd4651
mubaarock <mustophamubarack17@gmail.com>
2024-03-06 20:58:14 +1100

    Create a new foreignkey field with the User model

--- base/models.py
+++ base/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
 from django.utils.text import slugify
 
 
 # Create your models here.
 class Photo(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True
+    )
     short_title = models.CharField(max_length=15)
     slug = models.SlugField(blank=True)
     image = models.ImageField(max_length=200)
