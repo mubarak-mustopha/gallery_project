e98a5a80969a6dca2460dc884ff8d41b472b42cf
mubaarock <mustophamubarack17@gmail.com>
2024-03-07 14:36:50 +1100

    Add pagination to The HomePageView

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.contrib import messages
 from django.views import View
@@ -12,10 +13,20 @@ from .models import Photo
 
 class HomeView(View):
     template_name = "home.html"
+    photo_list = Photo.objects.all()
 
     def get(self, request):
-        photos = Photo.objects.all()
-        return render(request, self.template_name, {"photos": photos})
+        page = request.GET.get("page") or 1
+        paginator = Paginator(self.photo_list, 8)
+        num_pages = paginator.num_pages
+        current_page = paginator.get_page(page)
+        context = {
+            "current_page": current_page,
+            "page_num": int(page),
+            "num_pages": num_pages,
+            "ind": range(1, num_pages + 1),
+        }
+        return render(request, self.template_name, context=context)
 
 
 @method_decorator(login_required, name="dispatch")
