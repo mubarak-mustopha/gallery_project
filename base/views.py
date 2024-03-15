98c0b80c70b3ae2c9761fb557b229e95e07770a4
mubaarock <mustophamubarack17@gmail.com>
2024-03-15 11:26:19 +1100

    Create a mixin class to reuse for getting paginated data from Photo Model

--- base/views.py
+++ base/views.py
@@ -13,20 +13,15 @@ from .models import Photo
 # Create your views here.
 
 
-class HomeView(View):
-    template_name = "home.html"
-    photo_list = Photo.objects.all()
-
-    def get(self, request):
+class PaginatePhotoModelMixin:
+    def get_context_data(self, page, per_page=8, queryset=Photo.objects.all()):
         # paginator
-        paginator = Paginator(self.photo_list, 8)
+        paginator = Paginator(queryset, per_page)
         num_pages = paginator.num_pages
         page_range = paginator.page_range
         # requested page
-        page = request.GET.get("page") or 1
         if int(page) not in page_range:
             page = num_pages
-
         current_page = paginator.get_page(page)
         # [(photo_obj,month)]
         photos_months_list = [
@@ -39,7 +34,20 @@ class HomeView(View):
             "page_range": page_range,
             "photos_months_list": photos_months_list,
         }
-        return render(request, self.template_name, context=context)
+
+        return context
+
+
+class HomeView(View, PaginatePhotoModelMixin):
+    template_name = "home.html"
+    photo_list = Photo.objects.all()
+
+    def get(self, request):
+        # requested page
+        page = request.GET.get("page") or 1
+        return render(
+            request, self.template_name, context=self.get_context_data(page=page)
+        )
 
 
 @method_decorator(login_required(login_url="login"), name="dispatch")
@@ -72,32 +80,14 @@ class PhotoDetailView(View):
         )
 
 
-class UserPhotoList(View):
+class UserPhotoList(View, PaginatePhotoModelMixin):
     def get(self, request, username):
         # get list of photos by username
         user = User.objects.get(username=username)
         user_photo_list = Photo.objects.filter(user=user)
-
-        # paginator
-        paginator = Paginator(user_photo_list, 8)
-        num_pages = paginator.num_pages
-        page_range = paginator.page_range
         # requested page
         page = request.GET.get("page") or 1
-        if int(page) not in page_range:
-            page = num_pages
+        context = self.get_context_data(page=page, queryset=user_photo_list)
+        context["heading_txt"] = f"Photos By {user.username}"
 
-        current_page = paginator.get_page(page)
-        # [(photo_obj,month)]
-        photos_months_list = [
-            (photo, get_month(photo.created.month)) for photo in current_page
-        ]
-        context = {
-            "current_page": current_page,
-            "page_num": int(page),
-            "num_pages": num_pages,
-            "page_range": page_range,
-            "photos_months_list": photos_months_list,
-            "heading_txt": f"Photos By {user.username}",
-        }
         return render(request, "photo-list.html", context)
